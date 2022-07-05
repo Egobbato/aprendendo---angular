6be79bea8f483390359d6939b0e7fdd8f7bf0a1c
Eduardo Gobbato <102391940+Egobbato@users.noreply.github.com>
2022-07-05 09:18:30 -0300

    Conhecendo a pasta shared

--- src/app/courses/course-module.ts
+++ src/app/courses/course-module.ts
@@ -3,20 +3,17 @@ import { CourseListComponent } from './course-list.component';
 import { CourseInfoComponent } from './course-info.component';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { ReplacePipe } from '../pipe/replace.pipe';
 import { CommonModule } from '@angular/common';
-import { StarComponent } from '../star/star.component';
+import { StarModule } from '../shared/component/star/star.module';
+import { AppPipeModule } from '../shared/pipe/app-pipe.module';
 
 @NgModule({
-  declarations: [
-    CourseListComponent,
-    CourseInfoComponent,
-    ReplacePipe,
-    StarComponent,
-  ],
+  declarations: [CourseListComponent, CourseInfoComponent],
   imports: [
     CommonModule,
     FormsModule,
+    StarModule,
+    AppPipeModule,
     RouterModule.forChild([
       {
         path: 'courses',
